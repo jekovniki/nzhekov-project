586f78530b5f207a9d89b142969025b2fefec8d4
Nikolay <jekovniki95@gmail.com>
2020-08-12 11:36:23 +0300

    sign in form is working

--- src/pages/sing-in/index.js
+++ src/pages/sing-in/index.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback, useContext } from 'react'
+import { withRouter, Redirect } from 'react-router-dom'
+import app from '../../base'
+import { AuthContext } from '../../Auth'
 import MainWrapper from '../../components/mainWrapper'
 import TwoColumnsLayout from '../../components/twoCol-layout'
 import styles from './index.module.css'
@@ -8,17 +11,39 @@ import Title from '../../components/title'
 
 
 
-const SignIn = () => {
-
+const SignIn = ({ history }) => {
+    const handleLogin = useCallback(
+        async event => {
+            event.preventDefault()
+            const { email, password } = event.target.elements
+            try {
+                await app
+                .auth()
+                .signInWithEmailAndPassword(email.value, password.value)
+                history.push("/")
+            } catch (error) {
+                alert(error)
+            }
+        }, 
+        [history]
+    )
+    
     const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [currentUser, setCurrentUser] = useState(null);
+
+    if (currentUser) {
+        return <Redirect to="/" />
+    }
 
     return (
         <MainWrapper>
             <TwoColumnsLayout image={'https://images.unsplash.com/photo-1590099543482-3b3d3083a474?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=648&q=80'}>
-                <form className={styles[`form-container`]}>
+                <form className={styles[`form-container`]} onSubmit={handleLogin}>
                     <Title title="Sign In" />
                     <Input value={username} onChange={(e) => setUsername(e.target.value)} label="Username" id="username" />
+                    <Input value={email} onChange={(e) => setEmail(e.target.value)} label="E-mail" id="email" />
                     <Input value={password} onChange={(e) => setPassword(e.target.value)} label="Password" id="password" />
                     <MainBtn type="submit" title="Login" />
                 </form>
